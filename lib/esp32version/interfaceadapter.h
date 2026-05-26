1feb8987b64708714387810ff9d731319dd3e5c2
Victor <vicaraxd@gmail.com>
2026-05-26 17:04:25 +0200

    fix: Changed env and solved function return type

--- lib/esp32version/interfaceadapter.h
+++ lib/esp32version/interfaceadapter.h
@@ -9,11 +9,13 @@ public:
 
   inline void print (const std::string_view &s) {}
 
-  inline std::string userLineInput () {}
+  inline std::string userLineInput () {return "";}
 
   inline void sleepMillis (uint t) {delay(t);}
 
   inline ulong realTimeMicros () {return micros();}
 
   inline void playAudio (AudioPacket&& ap) {}
+
+  inline void informEmuRate (float r) {Serial.println(r);};
 };
