e91bf6752807683f45ebf10d8a29f072f5e93271
Victor <vicaraxd@gmail.com>
2026-06-05 13:14:53 +0200

    Transformed the project into a library

--- lib/pcversion/interfaceadapter.cpp
+++
@@ -1,9 +0,0 @@
-#include "interfaceadapter.h"
-#include "audio/audiostate.h"
-
-
-
-PCInterface::PCInterface()
-  : audio_stream(gb::SAMPLE_RATE)
-  , ini_t(std::chrono::steady_clock::now())
-{}
--- test/emulator/test_instr/misc/misc.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void runAllMiscInstrTests ();
-
-void test_instr_DAA ();
